b10bde01cf17251955462707ea6e9dd828f16bbd
xianren <2517230090@qq.com>
2017-12-04 16:20:41 +0800

    being perfect

--- app/src/main/java/defineview/FatherView.java
+++ app/src/main/java/defineview/FatherView.java
@@ -2,6 +2,7 @@ package defineview;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -28,18 +29,39 @@ public class FatherView extends ViewGroup {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+//        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 //        int childCount = getChildCount();
 //        for (int x = 0; x < childCount; x++) {
 //            View view = getChildAt(x);
 //            measureChild(view,widthMeasureSpec,heightMeasureSpec);
 //            setMeasuredDimension(view.getMeasuredWidth(),view.getMeasuredHeight());
 //        }
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+
+        int sizeWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int sizeHeight = MeasureSpec.getSize(heightMeasureSpec);
+        switch (widthMode) {
+            case MeasureSpec.AT_MOST:
+                Log.i("at_most", "atmost");
+                break;
+            case MeasureSpec.EXACTLY:
+                Log.i("EXACTLY", "EXACTLY");
+                break;
+        }
         measureChildren(widthMeasureSpec, heightMeasureSpec);
         //测量并保存layout的宽高(使用getDefaultSize时，wrap_content和match_perent都是填充屏幕)
         //稍后会重新写这个方法，能达到wrap_content的效果
-        setMeasuredDimension(getDefaultSize(getSuggestedMinimumWidth(), widthMeasureSpec),
-                getDefaultSize(getSuggestedMinimumHeight(), heightMeasureSpec));
+        int total = 0;
+        int measureWidth;
+        for (int i = 0; i < getChildCount(); i++) {
+            View view = getChildAt(i);
+            measureWidth = view.getMeasuredWidth();
+            total += measureWidth;
+        }
+        setMeasuredDimension((widthMode == MeasureSpec.EXACTLY) ? sizeWidth
+                : total, (heightMode == MeasureSpec.EXACTLY) ? sizeHeight
+                : sizeHeight);
 
     }
 
@@ -48,17 +70,17 @@ public class FatherView extends ViewGroup {
         int hadUsedHorizontal = 0;//水平已经使用的距离
         int hadUsedVertical = 0;//垂直已经使用的距离
         int width = getMeasuredWidth();
-//        int height = getMeasuredHeight();
+//      int height = getMeasuredHeight();
         for (int x = 0; x < getChildCount(); x++) {
             View view = getChildAt(x);
             //判断是否已经超出宽度
             if (view.getMeasuredWidth() + hadUsedHorizontal > width) {
                 //已经超出了宽度
-                hadUsedVertical = hadUsedVertical + view.getMeasuredHeight() + 10;
+                hadUsedVertical = hadUsedVertical + view.getMeasuredHeight();
                 hadUsedHorizontal = 0;
             }
             view.layout(hadUsedHorizontal, hadUsedVertical, hadUsedHorizontal + view.getMeasuredWidth(), hadUsedVertical + view.getMeasuredHeight());
-            hadUsedHorizontal = hadUsedHorizontal + 10 + view.getMeasuredWidth();
+            hadUsedHorizontal = hadUsedHorizontal + view.getMeasuredWidth();
         }
 
     }
--- app/src/main/java/defineview/SubView.java
+++ app/src/main/java/defineview/SubView.java
@@ -1,13 +1,18 @@
 package defineview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
+
+import com.xianren.customview.R;
 
 import static android.content.ContentValues.TAG;
 
@@ -16,8 +21,15 @@ import static android.content.ContentValues.TAG;
  */
 public class SubView extends View {
     Paint mPaint;
+    Paint txPaint;
     int widthSize;
     int heightSize;
+
+    String mTitleText;
+    private int mTitleTextColor;
+    private int mTitleTextSize;
+
+    private Rect mBound;
 
     public SubView(Context context) {
         super(context);
@@ -26,11 +38,14 @@ public class SubView extends View {
 
     public SubView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        getAttrubutSet(context, R.styleable.SubView_titleText, attrs);
         init();
     }
 
+
     public SubView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        getAttrubutSet(context, defStyleAttr, attrs);
         init();
     }
 
@@ -43,33 +58,57 @@ public class SubView extends View {
         Log.e(TAG, "onMeasure--widthMode-->" + widthMode);
         switch (widthMode) {
             case MeasureSpec.EXACTLY:
-
+                Log.e(TAG, "onMeasure--widthMode-->" + widthMode);
                 break;
             case MeasureSpec.AT_MOST:
-
+                Log.e(TAG, "onMeasure--widthMode-->" + widthMode);
                 break;
             case MeasureSpec.UNSPECIFIED:
-
+                Log.e(TAG, "onMeasure--widthMode-->" + widthMode);
                 break;
         }
     }
 
     @Override
-    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        super.onLayout(changed, left, top, right, bottom);
-
-    }
-
-    @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        int circleRadius = heightSize/2;
+        int circleRadius = heightSize / 2;
         mPaint.setAntiAlias(false);
         mPaint.setColor(Color.RED);
-        canvas.drawCircle(widthSize/2,heightSize/2, circleRadius, mPaint);
+        canvas.drawCircle(widthSize / 2, heightSize / 2, circleRadius, mPaint);
+        if (mTitleText != null)
+            canvas.drawText(mTitleText, getWidth() / 2 - mBound.width() / 2, getHeight() / 2 + mBound.height() / 2, txPaint);
     }
 
     void init() {
         mPaint = new Paint();
+        txPaint = new Paint();
+        mPaint.setColor(Color.GREEN);
+        txPaint.setTextSize(36);
+        mBound = new Rect();
+    }
+
+    void getAttrubutSet(Context context, int defstyle, AttributeSet attrs) {
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.SubView, defstyle, 0);
+        int n = a.getIndexCount();
+        for (int i = 0; i < n; i++) {
+            int attr = a.getIndex(i);
+            switch (attr) {
+                case R.styleable.SubView_titleText:
+                    mTitleText = a.getString(attr);
+                    break;
+                case R.styleable.SubView_titleTextColor:
+                    // 默认颜色设置为黑色
+                    mTitleTextColor = a.getColor(attr, Color.BLACK);
+                    break;
+                case R.styleable.SubView_titleTextSize:
+                    mTitleTextSize = a.getDimensionPixelSize(attr, (int) TypedValue.applyDimension(
+                            TypedValue.COMPLEX_UNIT_SP, 16, getResources().getDisplayMetrics()));
+                    break;
+
+            }
+
+        }
+        a.recycle();
     }
 }
